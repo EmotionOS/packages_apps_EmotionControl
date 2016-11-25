abc60881e785a87398cb3c9cf1b55c410713dad5
pafcholini <nadyaivanova14@gmail.com>
2016-11-25 12:12:30 +0100

    EC: fix my stupidity
    Signed-off-by: pafcholini <nadyaivanova14@gmail.com>

--- src/com/emotion/emotioncontrol/settings/SecureSettingSwitchPreference.java
+++ src/com/emotion/emotioncontrol/settings/SecureSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.emotionm.emotioncontrol.settings;
+package com.emotion.emotioncontrol.settings;
 
 import android.content.Context;
 import android.preference.SwitchPreference;
