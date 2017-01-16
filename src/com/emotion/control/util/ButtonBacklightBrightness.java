52123967a95242d6bdfd6b600cce36e57184cb1d
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-16 19:11:23 +0800

    EC: Preview timeout once user stops dragging
    Change-Id: I0abebe1041b6ad42b10e7d24f76ea17046e64666
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- src/com/emotion/control/util/ButtonBacklightBrightness.java
+++ src/com/emotion/control/util/ButtonBacklightBrightness.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,6 +61,8 @@ public class ButtonBacklightBrightness extends DialogPreference implements
 
     private ContentResolver mResolver;
 
+    private int mOriginalTimeout;
+
     public ButtonBacklightBrightness(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -106,7 +109,8 @@ public class ButtonBacklightBrightness extends DialogPreference implements
         mTimeoutValue = (TextView) view.findViewById(R.id.timeout_value);
         mTimeoutBar.setMax(30);
         mTimeoutBar.setOnSeekBarChangeListener(this);
-        mTimeoutBar.setProgress(getTimeout());
+        mOriginalTimeout = getTimeout();
+        mTimeoutBar.setProgress(mOriginalTimeout);
         handleTimeoutUpdate(mTimeoutBar.getProgress());
 
         ViewGroup buttonContainer = (ViewGroup) view.findViewById(R.id.button_container);
@@ -158,6 +162,7 @@ public class ButtonBacklightBrightness extends DialogPreference implements
         super.onDialogClosed(positiveResult);
 
         if (!positiveResult) {
+            applyTimeout(mOriginalTimeout);
             return;
         }
 
@@ -318,7 +323,7 @@ public class ButtonBacklightBrightness extends DialogPreference implements
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-        // Do nothing here
+        applyTimeout(seekBar.getProgress());
     }
 
     private static class SavedState extends BaseSavedState {
